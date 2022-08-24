eb661d559ca9980be630ad0cc006796713d9c405
Bharani619 <bharanish619@gmail.com>
2022-08-24 09:46:23 +0530

    initial check

---
+++ src/pages/MainRoutes.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+
+export const MainRoutes = () => {
+  return (
+    <Routes>
+        <Route />
+        <Route/>
+        <Route/>
+        <Route/>
+        <Route/>
+ 
+    </Routes>
+  )
+}
