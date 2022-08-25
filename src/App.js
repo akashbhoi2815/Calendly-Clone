599ad614d09d62a4a47ad37bc64abb5662c83559
Pavan Ambulkar <101567015+ambulkarpavan@users.noreply.github.com>
2022-08-25 14:15:42 +0530

    Updateded data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 
 import './App.css';
-import { MainRoutes } from './pages/MainRoutes';
+import  {MainRoutes}  from './Pages/MainRoutes';
 
 
 function App() {
   return (
     <div className="App">
-      <MainRoutes/>
+      <MainRoutes />
     </div>
   );
 }
