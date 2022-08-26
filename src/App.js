3b8c285812675b61d0985bfbdd30275c0da3f318
Chhattoo25 <95302680+Chhattoo25@users.noreply.github.com>
2022-08-26 22:34:17 +0530

    calendly HomePage day-4

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 
 import './App.css';
+import CalendlyNav2 from './components/Calendly/CalendlyNav2';
+import CalendlyNavbar from './components/Calendly/CalendlyNavbar';
+import EventTypes from './components/Calendly/EventTypes';
 
 import  {MainRoutes}  from './Pages/MainRoutes';
 
@@ -8,6 +11,7 @@ function App() {
   return (
     <div className="App">
       <MainRoutes />
+     
     </div>
   );
 }
