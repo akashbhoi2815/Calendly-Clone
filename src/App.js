cb9129474c755164eb6309f6edcde79151cb8dbb
Chhattoo25 <95302680+Chhattoo25@users.noreply.github.com>
2022-08-26 14:12:42 +0530

    Calendly nav1 done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 
 import './App.css';
+import CalendlyNav2 from './components/Calendly/CalendlyNav2';
+import CalendlyNavbar, { Navbar } from './components/Calendly/CalendlyNavbar';
 
 import  {MainRoutes}  from './Pages/MainRoutes';
 
@@ -7,7 +9,9 @@ import  {MainRoutes}  from './Pages/MainRoutes';
 function App() {
   return (
     <div className="App">
-      <MainRoutes />
+      {/* <MainRoutes /> */}
+      <CalendlyNavbar/>
+      <CalendlyNav2/>
     </div>
   );
 }
---
+++ src/components/Calendly/CalendlyNav2.jsx
@@ -0,0 +1,13 @@
+import { Box,useColorModeValue } from '@chakra-ui/react'
+import React from 'react'
+
+const CalendlyNav2 = () => {
+  return (
+    <Box px={4} my={'5rem'} >
+
+
+    </Box>
+  )
+}
+
+export default CalendlyNav2
